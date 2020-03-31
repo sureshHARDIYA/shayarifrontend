feda7049c585806d547b1cac50da9e2f5aee45f9
sureshHARDIYA <itsmeskm99@gmail.com>
2020-03-31 14:21:09 +0200

    update endpoints

--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
 
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { setContext } from 'apollo-link-context'
-import { AUTH_TOKEN } from './constants'
-import ScrollToTop from './components/ScrollToTop';
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import { AUTH_TOKEN } from "./constants";
+import ScrollToTop from "./components/ScrollToTop";
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql'
-})
+  uri: "https://eple.hib.no/intromat/graphql"
+});
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(AUTH_TOKEN)
+  const token = localStorage.getItem(AUTH_TOKEN);
 
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ''
+      authorization: token ? `Bearer ${token}` : ""
     }
-  }
-})
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
-})
+});
 
 ReactDOM.render(
   <BrowserRouter>
-  <ApolloProvider client={client}>
-    <ScrollToTop />
-    <App />
-  </ApolloProvider>
+    <ApolloProvider client={client}>
+      <ScrollToTop />
+      <App />
+    </ApolloProvider>
   </BrowserRouter>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
 serviceWorker.unregister();
