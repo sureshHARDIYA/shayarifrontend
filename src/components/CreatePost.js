d8a5707accffda4ee08a670c41f67864bd268a08
sureshHARDIYA <itsmeskm99@gmail.com>
2020-03-31 17:05:18 +0200

    Remove extra things

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Mutation, graphql, withApollo } from "react-apollo";
+import { graphql, withApollo } from "react-apollo";
 import gql from "graphql-tag";
 import { Form, Input, Button, Select, message } from "antd";
 import compose from "lodash/fp/compose";
@@ -74,50 +74,6 @@ class CreateLink extends Component {
             </Button>
           </Form.Item>
         </Form>
-
-        <Mutation
-          mutation={POST_MUTATION}
-          onCompleted={() => {
-            message.success("The shayari was created Succesfully!");
-            this.props.history.push("/");
-          }}
-        >
-          {(postMutation, { loading, data }) => (
-            <Form
-              onFinish={values => {
-                postMutation({ variables: values });
-              }}
-              onSubmit={this.onSubmit}
-            >
-              <Form.Item name="body" label="Body" rules={[{ required: true }]}>
-                <Input.TextArea style={{ minHeight: "240px" }} />
-              </Form.Item>
-              <Form.Item
-                name="tags"
-                label="Tags"
-                hasFeedback
-                rules={[
-                  { required: true, message: "Please select your country!" }
-                ]}
-              >
-                <Select placeholder="Please select a tags" mode="tags">
-                  {Array.isArray(this.props.data.tags) &&
-                    this.props.data.tags &&
-                    this.props.data.tags.map(item => (
-                      <Option value={item.title} key={item.id}>
-                        {item.title}
-                      </Option>
-                    ))}
-                </Select>
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
-          )}
-        </Mutation>
       </div>
     );
   }
